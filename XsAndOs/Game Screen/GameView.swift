6298bc67e8552721062c31a50ed15de66fe521fb
SadeEtensh <eteneshsade550305@gmail.com>
2023-04-17 17:42:47 +0300

    Update on Game Screen

--- XsAndOs/Game Screen/GameView.swift
+++ XsAndOs/Game Screen/GameView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct GameView: View {
     @Environment(\.dismiss) var dismiss
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello, Hello!")
     }
 }
 
