794ef7d2940510b0c39000cbc782a708f2008a9c
SadeEtensh <eteneshsade550305@gmail.com>
2023-04-17 12:35:09 +0300

    Initial Commit

---
+++ XsAndOs/XsAndOsApp.swift
@@ -0,0 +1,17 @@
+//
+//  XsAndOsApp.swift
+//  XsAndOs
+//
+//  Created by Sade on 17/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct XsAndOsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
